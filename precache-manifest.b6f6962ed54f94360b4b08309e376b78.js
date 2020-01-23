c80d544e0bc48df45d7f01ff7204cc888ac0e618
Alexander <alexanderamirez002@gmail.com>
2020-01-22 21:28:54 -0400

    Updates

---
+++ precache-manifest.b6f6962ed54f94360b4b08309e376b78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c29a87c82e6146123dece0bf4a96e1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "45681c1a655112079427",
+    "url": "/monsters-rolodex/static/css/main.2d797d30.chunk.css"
+  },
+  {
+    "revision": "76bff24cf0b4e47bb281",
+    "url": "/monsters-rolodex/static/js/2.ff2f932d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.ff2f932d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "45681c1a655112079427",
+    "url": "/monsters-rolodex/static/js/main.4dab5dd5.chunk.js"
+  },
+  {
+    "revision": "72e8d3a2488c011ed91b",
+    "url": "/monsters-rolodex/static/js/runtime-main.2b4c7610.js"
+  }
+]);
